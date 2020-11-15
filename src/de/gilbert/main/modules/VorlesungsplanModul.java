10fca4babc911d7c3c393f3e7392da61e820bd0e
zusibell <56092294+zusibell@users.noreply.github.com>
2020-11-15 11:31:31 +0100

    Update VorlesungsplanModul.java

--- src/de/gilbert/main/modules/VorlesungsplanModul.java
+++ src/de/gilbert/main/modules/VorlesungsplanModul.java
@@ -4,8 +4,8 @@ import de.gilbert.main.Verweismodul;
 
 import java.net.URL;
 
-public class KalenderModul extends Verweismodul {
-    public KalenderModul(URL link, String platzhalter) {
+public class VorlesungsplanModul extends Verweismodul {
+    public VorlesungsplanModul(URL link, String platzhalter) {
         super(link, platzhalter);
     }
 }
